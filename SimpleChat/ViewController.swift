a94c297f99ba9cd3e1707c25cfe6309804e3bf04
Jeremy Zhou <jeremyzhou@me.com>
2018-03-11 11:49:31 -0700

    Initial Commit

---
+++ SimpleChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleChat
+//
+//  Created by Jeremy Zhou on 3/11/18.
+//  Copyright © 2018 Jeremy Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
