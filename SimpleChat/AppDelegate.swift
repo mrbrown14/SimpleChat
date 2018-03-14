f4b6683ad914ff89e14757644ce928379e3bf7b0
Jeremy Zhou <jeremyzhou@me.com>
2018-03-13 22:39:41 -0700

    add fps counter

--- SimpleChat/AppDelegate.swift
+++ SimpleChat/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        FPSCounter.showInStatusBar(UIApplication.shared)
+        FPSCounter.showInStatusBar(UIApplication.shared)
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.backgroundColor = UIColor.white
